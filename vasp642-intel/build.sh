bf0a02061102706c97004f1f429c7500d844895d
yushengyangchem <yushengyangchem@gmail.com>
2026-02-28 18:26:39 +0800

    chore(vasp): rm proxy var

--- vasp642-intel/build.sh
+++ vasp642-intel/build.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-unset no_proxy ftp_proxy http_proxy https_proxy all_proxy rsync_proxy
-unset NO_PROXY FTP_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY RSYNC_PROXY
 sudo apptainer build vasp642_intel.sif vasp642_intel.def
